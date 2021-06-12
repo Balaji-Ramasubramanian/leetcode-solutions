bd71780a8516454284f10e26b518f1ab6af94915
balaji030698 <balaji030698@gmail.com>
2021-06-12 23:49:14 +0530

    10. Regular Expression Matching

--- hard/RegularExpressionMatching.java
+++ hard/RegularExpressionMatching.java
@@ -3,6 +3,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class RegularExpressionMatching {
+
+	// Using Pattern class & Matcher class to check the regex matching for given string
     public boolean isMatch(String s, String p) {
 	    Pattern r = Pattern.compile(p);
 	    Matcher m = r.matcher(s);
