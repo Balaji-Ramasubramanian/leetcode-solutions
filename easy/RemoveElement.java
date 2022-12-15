f4facdd2ee9fa782832ee99c7921b5cc91c1e587
Balaji <balaji030698@gmail.com>
2022-12-15 15:23:43 +0530

    27. Remove Element

--- easy/RemoveElement.java
+++ easy/RemoveElement.java
@@ -1,22 +1,31 @@
 // Problem URL: https://leetcode.com/problems/remove-element/
-
-
+/**
+ * 
+ * Step 1: Iterate from index 0 to lastDigit
+ * Step 2: Check if i and given element are same,
+ * Step 3: If those are same, push the element to lastDigit and move other elements in the array forward.
+ * Step 4: Decrease lastDigit by one in every push action. Return the lastDigit at last. 
+ *         This denotes the last Digit of non-duplicate elements in the array
+ **/
 class RemoveElement {
-public int removeElement(int[] nums, int val) {
-        if(nums.length == 0)
-            return 0;
-        int lastpos = nums.length-1;
-        for(int i =0;i<nums.length && i<lastpos;i++){
+    int lastDigit = 0;
+    public int removeElement(int[] nums, int val) {
+        lastDigit = nums.length;
+        for(int i =0; i<= nums.length-1 && i<lastDigit; i++){
             if(nums[i] == val){
-                while (nums[lastpos] == val && lastpos >i)
-                    lastpos--;
-                int temp = nums[i];
-                nums[i] = nums[lastpos];
-                nums[lastpos] = temp;
-                if(lastpos>0)
-                    lastpos--;
+                pushAtLast(nums, i);
+                i--;
             }
         }
-        return lastpos >= 0 && nums[lastpos] == val ? lastpos: lastpos+1;
+        return lastDigit;
+    }
+
+    public void pushAtLast(int[] nums, int i){
+        int temp = nums[i];
+        for(; i<lastDigit-1; i++){
+            nums[i] = nums[i+1];
+        }
+        nums[lastDigit-1] = temp;
+        lastDigit--;
     }
 };
